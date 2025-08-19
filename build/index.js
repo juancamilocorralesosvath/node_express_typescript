24241c14e6d8234a0cf728773c7625b695daa05a
juancamilocorralesosvath <juancamilocorra@gmail.com>
2025-08-19 12:41:59 -0500

    initialized node + ts project, but problem is arising

---
+++ build/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+const app = express();
+app.use(express.json());
+const PORT = 3000;
+app.get('/ping', (_req, res) => {
+    console.log('un ping!' + new Date().toLocaleDateString());
+    res.send('pong');
+});
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`);
+});
+//# sourceMappingURL=index.js.map
